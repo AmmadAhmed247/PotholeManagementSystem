e42b3eb7fc59b7ea160c43a3411de8f659591686
Ammad Ahmed <ammadwork123@gmail.com>
2025-10-17 01:33:03 +0500

    Fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold underline">Hello, Vite + React!</h1>
-    </div>
+    <div>App</div>
   )
 }
 
